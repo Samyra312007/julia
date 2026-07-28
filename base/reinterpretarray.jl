d178cc0c62d7a58d0bbac66e984f55762e40908a
Sahil Kumar <samayra312007@gmail.com>
2026-07-28 18:22:34 +0530

    Update base/reinterpretarray.jl
    Co-authored-by: Jameson Nash <vtjnash@gmail.com>

--- base/reinterpretarray.jl
+++ base/reinterpretarray.jl
@@ -452,7 +452,7 @@ end
     @boundscheck checkbounds(a, inds...)
     li = _to_linear_index(a, inds...)
     ap = cconvert(Ptr{T}, a)
-    p = unsafe_convert(Ptr{T}, ap) + aligned_sizeof(T) * (li - 1)
+    p = unsafe_convert(Ptr{T}, ap) + elsize(a) * (li - 1)
     GC.@preserve ap return unsafe_load(p)
 end
 
